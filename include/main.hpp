ddd4d47398266d1fe76fa2f9c3469ff3b9e2858c
BusyBoredom <cwilkin@protonmail.com>
2019-04-12 00:00:20 -0400

    Implemented context setter for beta, rload, etc.

---
+++ include/main.hpp
@@ -0,0 +1,6 @@
+
+void set_context();
+  // Set the appropriate static member variables in the amplifier class.
+
+void generate_starting_pop();
+  // Generates random starting population.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include "../include/amplifier.cpp"
 
+const int beta = 250;
+const float Vt = 0.026;  // Volts
+const int Rload = 1000;  // Ohms
+const float VBE = 0.7;  // Volts
+const int Rsource = 1000;  // Ohms
+
+const int generations = 1000;
+const int population = 1000;
+
 int main() {
-  return 0;
+  set_context();
+  Amplifier pop[population];
+}
+
+void set_context() {
+  // Set the appropriate static member variables in the amplifier class.
+  Amplifier::beta = beta;
+  Amplifier::Vt = Vt;
+  Amplifier::Rload = Rload;
+  Amplifier::VBE = VBE;
+  Amplifier::Rsource = Rsource;
 }
